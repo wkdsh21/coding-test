86ea47b6247463d8983046b7525e0a4fb989a465
wkdsh21 <100358810+wkdsh21@users.noreply.github.com>
2022-11-22 17:57:13 +0900

    Add files via upload

--- web/test.js
+++ web/test.js
@@ -1,6 +1,8 @@
 nav1.addEventListener('click', function(){scrollMove("presentation")});
 nav2.addEventListener('click', function(){scrollMove("algorithm")});
 nav3.addEventListener('click', function(){scrollMove("project")});
+up.addEventListener('click', function(){scrollMove("presentation")});
+
 function scrollMove(id){
     let location = document.querySelector(`#${id}`).offsetTop;
     window.scrollTo({ top: location, behavior: "smooth" });
